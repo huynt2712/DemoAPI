eb1579ab7d62b1ded32ae49324235c0b1a602aaf
hungtd <hungtd@hmsfirst.com>
2022-05-21 16:05:50 +0700

    Edit db context

--- WebApiCodeFirstDB/Data/BlogDBContext.cs
+++ WebApiCodeFirstDB/Data/BlogDBContext.cs
@@ -9,20 +9,17 @@ namespace WebApiCodeFirstDB.Data
         public DbSet<Post> Posts { get; set; }
         public DbSet<PostCategory> Categories { get; set; }
 
-        protected readonly IConfiguration Configuration;
-
-        public BlogDBContext(IConfiguration configuration, DbContextOptions<BlogDBContext> options)
+        public BlogDBContext(DbContextOptions<BlogDBContext> options)
             : base(options)
         {
-            Configuration = configuration;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder
-            //    .UseLazyLoadingProxies()
+            optionsBuilder
+                .UseLazyLoadingProxies();
             //    .ConfigureWarnings(w => w.Ignore(CoreEventId.LazyLoadOnDisposedContextWarning));
-                //.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            //.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             //.UseSqlServer("Data Source=MSI;Initial Catalog=PostCategoryDB;User ID=sa;Password=1111;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
